76909c871d85d50f8b25a908c47316f8021bef17
patkdot <pat.kumm@gmail.com>
2025-10-03 12:51:14 +0200

    optimize setup and readme (#2)
    * optimize setup and readme

--- setup.sh
+++ setup.sh
@@ -7,4 +7,10 @@ ddev exec composer install
 ddev exec bin/console system:install --basic-setup --shop-locale=de-DE --force
 ddev exec composer require --dev shopware/dev-tools
 ddev exec APP_ENV=prod bin/console framework:demodata --orders=0
+echo "Create .env.local ..."
+cat <<EOF > shopware/.env.local
+APP_DEBUG=1
+APP_ENV=dev
+EOF
 ddev exec bin/console plugin:install -a KdotPlayground
+ddev exec bin/console kdot:import
